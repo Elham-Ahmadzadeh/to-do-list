1dedaf0f6274c672e3ef7c3fcc902457037a7573
Elham Ahmadzadeh <elham.ahmadzadeh@sigmatechnology.se>
2022-03-02 13:40:47 +0100

    added css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {FC} from 'react';
+import "./App.css";
 
-import './App.css';
-
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
+      <div className='header'>
+        <div className='inputContainer'>
+        <input placeholder='Task...'/>
+        <input type="number" placeholder='Deadline(in Days)'/>
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className='toDoList'></div>
     </div>
   );
 }
